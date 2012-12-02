83d628c49c308da4a506d0760cc9cf067ef430db
aschwa <aschwartz@eartheconomics.org>
2012-12-02 14:37:55 -0800

    added todo page, about page

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.about = function(req, res){
+  res.render('index', { title: 'About' })
+};
+
+exports.todo = function(req, res){
+  res.render('todo', {
+    title: 'New Todo List'
+  });
+};
